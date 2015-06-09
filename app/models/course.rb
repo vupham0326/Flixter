3528cce7129152647623ba2174389ccd6e5f5ca7
Vu Pham <vupham0326@mail.com>
2015-06-09 12:41:50 +0000

    made changes to add personal error messages to courses page

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
 	belongs_to :user
 
-	validates :title, :presence => true
-	validates :description, :presence => true
-	validates :cost, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
+	validates :title, :presence => {:message => "Title can't be blank"}
+	validates :description, :presence => {:message => "Description can't be blank"}
+	validates :cost, :presence => {:message => "Cost can't be blank"}, :numericality => {:greater_than_or_equal_to => 0}
 end
