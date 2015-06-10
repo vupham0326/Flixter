ad3792c30a6015298db127438dd1efa474861ba2
Vu Pham <vupham0326@mail.com>
2015-06-10 02:46:55 +0000

    building sections

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ActiveRecord::Base
 	belongs_to :user
-
+	has_many :sections
+	
 	validates :title, :presence => {:message => "Title can't be blank"}
 	validates :description, :presence => {:message => "Description can't be blank"}
 	validates :cost, :presence => {:message => "Cost can't be blank"}, :numericality => {:greater_than_or_equal_to => 0}
