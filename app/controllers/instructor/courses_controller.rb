2d6367ce28da25757d0e42b194e736221ae49920
Vu Pham <vupham0326@mail.com>
2015-06-16 00:30:16 +0000

    dealing with the reat of the permissions

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,27 +1,36 @@
 class Instructor::CoursesController < ApplicationController
-	before_action :authenticate_user!
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, :only => [:show]
 
-	def new
-		@course = Course.new
-	end
+  def new
+    @course = Course.new
+  end
 
-	def create
-		@course = current_user.courses.create(course_params)
-		if @course.valid?
-			redirect_to instructor_course_path(@course)
-		else
-			render :new, :status => :unprocessable_entity
-		end
-	end
+  def create
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, :status => :unprocessable_entity
+    end
+  end
 
-	def show
-		@course = Course.find(params[:id])
-	end
+  def show
+  end
 
-	private 
+  private
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render :text => "Unauthorized", :status => :unauthorized
+    end
+  end
 
-	def course_params
-		params.require(:course).permit(:title, :description, :cost)
-	end
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
 
+  def course_params
+    params.require(:course).permit(:title, :description, :cost)
+  end
 end
